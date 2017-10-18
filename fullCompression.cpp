3eeabd29805e4de7b9ddd620ad8bb1f2f7c7717a
Jason Jin <jasonjin@usc.edu>
2017-10-17 21:23:50 -0700

    COMPRESSING WORKS

--- fullCompression.cpp
+++ fullCompression.cpp
@@ -74,12 +74,80 @@ void quantizateDct(int** dct, int quantizationTable[N][N]) {
 	}
 }
 
+void quantizateDct(float** dct, int quantizationTable[N][N]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			dct[i][j] = round(dct[i][j] / quantizationTable[i][j]);
+		}
+	}
+}
+
 void unquantizateDct(int** dct, int quantizationTable[N][N]) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			dct[i][j] *= quantizationTable[i][j];
 		}
 	}
+}
+
+int** AlternativeDct(double** pixels, int quantizationTable[N][N]) {
+	const float coeff[7] = {
+		0.49039, 0.46194, 0.41573, 0.35355,
+		0.27779, 0.19134, 0.09755
+	};
+	float** tmp = new float*[N];
+	int** quant = new int*[N];
+	for(int i = 0; i < N; i++) {
+		tmp[i] = new float[N];
+		quant[i] = new int[N];
+		float s0 = (float) (pixels[i][0] + pixels[i][7]) - 256;
+		float s1 = (float) (pixels[i][1] + pixels[i][6]) - 256;
+		float s2 = (float) (pixels[i][2] + pixels[i][5]) - 256;
+		float s3 = (float) (pixels[i][3] + pixels[i][4]) - 256;
+
+		float d0 = (float) (pixels[i][0] - pixels[i][7]);
+		float d1 = (float) (pixels[i][1] - pixels[i][6]);
+		float d2 = (float) (pixels[i][2] - pixels[i][5]);
+		float d3 = (float) (pixels[i][3] - pixels[i][4]);
+
+		tmp[i][ 0]     = coeff[3]*(s0+s1+s2+s3);
+		tmp[i][1] = coeff[0]*d0+coeff[2]*d1+coeff[4]*d2+coeff[6]*d3;
+		tmp[i][2] = coeff[1]*(s0-s3)+coeff[5]*(s1-s2);
+		tmp[i][3] = coeff[2]*d0-coeff[6]*d1-coeff[0]*d2-coeff[4]*d3;
+		tmp[i][4] = coeff[3]*(s0-s1-s2+s3);
+		tmp[i][5] = coeff[4]*d0-coeff[0]*d1+coeff[6]*d2+coeff[2]*d3;
+		tmp[i][6] = coeff[5]*(s0-s3)+coeff[1]*(s2-s1);
+		tmp[i][7] = coeff[6]*d0-coeff[4]*d1+coeff[2]*d2-coeff[0]*d3;
+	}
+	for (int col = 0; col < 8; col++) {
+		float s0 = tmp[0][col] + tmp[7][col];
+		float s1 = tmp[1][col] + tmp[6][col];
+		float s2 = tmp[2][col] + tmp[5][col];
+		float s3 = tmp[3][col] + tmp[4][col];
+
+		float d0 = tmp[0][col] - tmp[7][col];
+		float d1 = tmp[1][col] - tmp[6][col];
+		float d2 = tmp[2][col] - tmp[5][col];
+		float d3 = tmp[3][col] - tmp[4][col];
+
+		tmp[0][col] = coeff[3]*(s0+s1+s2+s3);
+		tmp[1][col] = coeff[0]*d0+coeff[2]*d1+coeff[4]*d2+coeff[6]*d3;
+		tmp[2][col] = coeff[1]*(s0-s3)+coeff[5]*(s1-s2);
+		tmp[3][col] = coeff[2]*d0-coeff[6]*d1-coeff[0]*d2-coeff[4]*d3;
+		tmp[4][col] = coeff[3]*(s0-s1-s2+s3);
+		tmp[5][col] = coeff[4]*d0-coeff[0]*d1+coeff[6]*d2+coeff[2]*d3;
+		tmp[6][col] = coeff[5]*(s0-s3)+coeff[1]*(s2-s1);
+		tmp[7][col] = coeff[6]*d0-coeff[4]*d1+coeff[2]*d2-coeff[0]*d3;
+	}
+	quantizateDct(tmp,quantizationTable);
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			quant[i][j] = (int)tmp[i][j];
+		}
+		delete[] tmp[i];
+	}
+	delete[] tmp;
+	return quant;
 }
 
 // Inverse DCT
@@ -190,8 +258,16 @@ vector<unsigned char> CreateJPEGVector(vector<unsigned char> quant, vector<unsig
 	jpeg.push_back(0x00);
 	jpeg.push_back(0x01);
 	jpeg.push_back(0x01);
-	jpeg.push_back(0x44);
+	jpeg.push_back(0x11);
 	jpeg.push_back(0x00);
+	//Restart
+	/**jpeg.push_back(0xff);
+	jpeg.push_back(0xdd);
+	jpeg.push_back(0x00);
+	jpeg.push_back(0x04);
+	jpeg.push_back(0x00);
+	jpeg.push_back(0x04);**/
+
 	//Lum Huffman DC
 	vector<unsigned char> huffmanDC = GetHuffmanDC();
 	jpeg.insert(jpeg.end(), huffmanDC.begin(), huffmanDC.end());
@@ -242,6 +318,15 @@ int main(int argc, char** argv) {
 		{15, 17, 19, 21, 23, 25, 27, 29},
 		{17, 19, 21, 23, 25, 27, 29, 31}
 
+		/**{16, 11, 10, 16, 24, 40, 51, 61},
+		{12, 12, 14, 19, 26, 58, 60, 55},
+		{14, 13, 16, 24, 40, 57, 69, 56},
+		{14, 17, 22, 29, 51, 87, 80, 62},
+		{18, 22, 37, 56, 68, 109, 103, 77},
+		{24, 35, 55, 64, 81, 104, 113, 92},
+		{49, 64, 78, 87, 103, 121, 120, 101},
+		{72, 92, 95, 98, 112, 100, 103, 99}**/
+
 		/**{1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 1, 1, 1, 1, 1, 1, 1},
@@ -251,9 +336,30 @@ int main(int argc, char** argv) {
 		{1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 1, 1, 1, 1, 1, 1, 1}**/
 	};
+	
+	/**
+	int fixedNum[N][N] = {
+		{217,  -1,  -2, -82, -61, -24,  11,  11},
+		{-70, -16, -13, -11,  12,  29,  36,  12},
+		{-24,   8,  -8, -11,  -2,  21,   0,  -6},
+		{  0, -20, -21, -23,  -9,   7,   0,   2},
+		{ -3,   0,  18,  -3,  -4,   1,   2,   0},
+		{ -2,   0,   0,  -3,   4,   2,  -3,  -1},
+		{ -3,   1,   4,  -2,   0,   3,  -3,   0},
+		{ -2,   2,   0,  -2,   3,   0,   0,   1}
+
+	};
+	int** fixed = new int*[N];
+	for(int f = 0; f < N; f++) {
+		fixed[f] = new int[N];
+		for(int d = 0; d < N; d++) {
+			fixed[f][d] = fixedNum[f][d];
+		}
+	}**/
 
 	int h;
 	int w;
+	int previousDC;
 	for (h = 0; h <= height - 8; h += 8) {
 		for (w = 0; w <= width - 8; w += 8) {
 			double** pixels = new double*[N];
@@ -263,22 +369,44 @@ int main(int argc, char** argv) {
 
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					pixels[i][j] = ((double)grayscaleArray[h+i][w+j])-128;
+					pixels[i][j] = ((double)grayscaleArray[h+i][w+j]);
 				}
 			}
 
 			int** dct = generateDct(pixels, cosine, coefficient);
 			quantizateDct(dct, quantizationTable);
-
-			vector<bool> encode = HuffmanEncode(dct);
-			if(h == 24*8 && w == 19* 8) {
+			/**if(h == 0 && w == 0)
+			{
+				previousDC = dct[0][0];
+			}
+			else
+			{
+				int saveDC = dct[0][0];
+				dct[0][0] -= previousDC;
+				previousDC = saveDC;
+			}
+			vector<bool> encode = HuffmanEncode(dct);**/
+			int** alternativeDCT = AlternativeDct(pixels, quantizationTable);
+			if(h == 0 && w == 0)
+			{
+				previousDC = alternativeDCT[0][0];
+			}
+			else
+			{
+				int saveDC = alternativeDCT[0][0];
+				alternativeDCT[0][0] -= previousDC;
+				previousDC = saveDC;
+			}
+			vector<bool> encode = HuffmanEncode(alternativeDCT);
+			
+			if(h == 5*8 && w == 19* 8) {
 				for(int dctY = 0; dctY < N; dctY++) {
 					for(int dctX = 0; dctX < N; dctX++) {
-						//cout << dct[dctY][dctX] << " ";
+						cout << dct[dctY][dctX] << " ";
 					}
-					//cout << endl;
+					cout << endl;
 				}
-				//cout << endl;
+				cout << endl;
 				for(int m = 0; m < encode.size(); m++) {
 					cout << (int)encode[m];
 				}
